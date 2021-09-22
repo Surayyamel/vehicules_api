1eaad897ace1bc52bcb3ecab240aa67ad25bad55
Surayya <Surayyamel@users.noreply.github.com>
2021-09-22 15:41:58 +0100

    added DB request to return all table rows

--- routes/handlers/getAllVehicules.js
+++ routes/handlers/getAllVehicules.js
@@ -1,5 +1,14 @@
-const getAllVehicules = (req, res) => {
-    res.send('All Vehicules');
+const db = require('../../db/models/index');
+const Vehicule = db['Vehicule'];
+
+const getAllVehicules = async (req, res) => {
+    try {
+        const vehicules = await Vehicule.findAll();
+
+        res.status(200).json(vehicules);
+    } catch (error) {
+        res.status(500).json(error);
+    }
 }
 
 module.exports = getAllVehicules;
