f969aa20ce84ac0c1bb468e3de3a6390978b9623
Surayya <Surayyamel@users.noreply.github.com>
2021-10-06 11:03:31 +0100

    added improvements after interview

--- db/models/vehicule.js
+++ db/models/vehicule.js
@@ -6,8 +6,23 @@ module.exports = (sequelize, DataTypes) => {
   class Vehicule extends Model {};
 
   Vehicule.init({
+    id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+    },
     make: DataTypes.STRING,
-    model: DataTypes.STRING
+    model: DataTypes.STRING,
+    createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false
+    } 
+
   }, {
     sequelize,
     modelName: 'Vehicule',
--- routes/handlers/getAllVehicules.js
+++ routes/handlers/getAllVehicules.js
@@ -5,7 +5,17 @@ const getAllVehicules = async (req, res) => {
     try {
         const vehicules = await Vehicule.findAll();
 
-        res.status(200).json(vehicules);
+        const vehiculesArrayWithString = [];
+
+        for (let i = 0; i < vehicules.length; i++) {
+            const vehicule = vehicules[i].toJSON();
+            vehicule.string = `${vehicules[i].make}-${vehicules[i].model}`;
+            vehiculesArrayWithString.push(vehicule);
+        }
+
+
+        console.log(vehiculesArrayWithString)
+        res.status(200).json(vehiculesArrayWithString);
     } catch (error) {
        
         res.status(500).json(error.message);
